9318a0102d24a871b47e0c6205becfdb656168f3
omar <omeldone@gmail.com>
2022-10-21 01:22:17 +0700

    finsh logic and js

--- main.js
+++ main.js
@@ -1,37 +1,61 @@
 //get al sections
 let allSections = document.querySelectorAll(".section");
-
-//submit btn
-
-let submit =document.querySelector(".submit");
-let valid=false;
+let submit = document.querySelector(".submit");
+let input = document.querySelector("input");
 let alert =document.querySelector(".alert")
+let cityName=document.querySelector(".city-name");
+let weatherState=document.querySelector(".weather-state");
+let weatherDegree= document.querySelector(".weather-degree");
+let searching = document.querySelector(".searching");
+
+
 //back icon
 let back =document.querySelector(".back");
 submit.addEventListener("click",function () {
-  if(valid){
-     hiddenSection();
-  }else{
-//     <small class="w-full p-2 font-bold capitalize rounded-md bg-red-200 text-red-500">pleas enter a valid city</small>
-  }
+      searching.classList.toggle("hidden");
+      getDataApi();
+      clear();
 });
+
 back.addEventListener("click",hiddenSection);
 
+//fecth weather
 let apiKey = "2138f0dbbcec163a4b8a44a0215a3bd6";
-let city="cairo";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q="cairo"&appid=${apiKey}`
+
+function getDataApi() {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`).then(
+    res => res.json()
+  ).then(
+    data => {
+       handelData(data)
+    }
+  )
+}
+
+
+function handelData(data){
+     if(data.cod != "404"){
+       searching.classList.toggle("hidden");
+       weatherDegree.innerHTML = ((5/9)*data.main.temp-32).toFixed(2);
+       cityName.innerHTML = data.name;
+       weatherState.innerHTML = data.weather[0].description;
+       hiddenSection();
+      console.log(data);
+    }else {
+       searching.innerHTML = "please enter valid city";
+       searching.classList.toggle("bg-red-200");
+       searching.classList.toggle("text-red-500");
+    }
+}
 
 function hiddenSection() {
   allSections.forEach((section, i) => {
          section.classList.toggle("hidden");
   });
-
 }
 
-fetch(apiUrl).then(
-  res => res.json()
-).then(
-  data => {
-     console.log(data);
-  }
-)
+function clear() {
+   input.value ='';
+   searching.classList.toggle("bg-red-200");
+   searching.classList.toggle("text-red-500");
+}
